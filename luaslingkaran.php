db60fbe1583123b5e3178ab294090c1bb424e61f
Bayu <if24.bayupamungkas@mhs.ubpkarawang.ac.id>
2025-11-14 19:59:31 +0700

    Add files via upload

---
+++ luaslingkaran.php
@@ -0,0 +1,32 @@
+<?php
+
+class LuasLingkaran {
+    public const phi = 3.14;
+    protected int  $Jari;
+
+    public function __constucrt($isijari =1){
+        
+    }
+
+    public fungction tampil() {
+        $rumus = LuasLingkaran::phi * $this->$Jari*$this->$Jari
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}" ;
+    }
+
+    public static function_destruct() {
+        echo "ini dari static";
+    }
+
+    public static function_destruct() {
+        echo"u'dah ah cape"
+    }
+}
+
+$Lingkaran = new LuasLingkaran();
+$Lingkaran->$Jari = 5;
+$Lingkaran-> tampil('ban');
+
+LuasLingkaran::testing();
+echo".."
+
+?>
